768a44a3bf70e1a2a5efe18f56c541293b0f78d2
vhbesc129 <110871813+vhbesc129@users.noreply.github.com>
2022-08-12 17:25:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Animate infinity</title>
+</head>
+<body>
+    <div class="menu">
+        <div class="menu1">
+            <h2 class="texto">Monitoração</h2>
+            <ul>
+                <li>Monitoramentos</li>
+                <li>Plantões</li>
+                <li>Ocorrências</li>
+                <li>WikiPrd</li>
+                <li>Salas Online</li>
+            </ul>
+        </div>
+    </div>
+
+    <section>
+        
+        <div class="box">
+            <h2>Login</h2>
+            <form method="POST">
+                <div class="inputBox">
+                    <input type="text" name="" required="" name="username">
+                    <label>Username</label>
+                </div>
+                <div class="inputBox">
+                    <input type="password" name="" required="" name="senha">
+                    <label>Password</label>
+                </div>
+                <input type="submit" name="" value="Submit">
+                <p>Ainda não inscrito? <a href="cadastrar.php">Cadastre-se</a></p> 
+            </form>
+        </div>
+
+        <div class="box1">
+            <h2>Login Salas</h2>
+            <form method="POST">
+                <div class="inputBox1">
+                    <input type="text1" name="" required="">
+                    <label>Username</label>
+                </div>
+                <div class="inputBox1">
+                    <input type="password1" name="" required="">
+                    <label>Password</label>
+                </div>
+                <input type="submit" name="" value="Submit1">
+                <p>Ainda não inscrito? <a href="cadastrar1.php">Cadastre-se</a></p> 
+            </form>
+        </div>
+        <div class="container">
+            <div class="circle">
+                <span style="--i:0;"></span>
+                <span style="--i:1;"></span>
+                <span style="--i:2;"></span>
+                <span style="--i:3;"></span>
+                <span style="--i:4;"></span>
+                <span style="--i:5;"></span>
+                <span style="--i:6;"></span>
+                <span style="--i:7;"></span>
+                <span style="--i:8;"></span>
+                <span style="--i:9;"></span>
+                <span style="--i:10;"></span>
+                <span style="--i:11;"></span>
+                <span style="--i:12;"></span>
+                <span style="--i:13;"></span>
+                <span style="--i:14;"></span>
+                <span style="--i:15;"></span>
+                <span style="--i:16;"></span>
+                <span style="--i:17;"></span>
+                <span style="--i:18;"></span>
+                <span style="--i:19;"></span>
+                <span style="--i:20;"></span>
+            </div>
+            <div class="circle">
+                <span style="--i:0;"></span>
+                <span style="--i:1;"></span>
+                <span style="--i:2;"></span>
+                <span style="--i:3;"></span>
+                <span style="--i:4;"></span>
+                <span style="--i:5;"></span>
+                <span style="--i:6;"></span>
+                <span style="--i:7;"></span>
+                <span style="--i:8;"></span>
+                <span style="--i:9;"></span>
+                <span style="--i:10;"></span>
+                <span style="--i:11;"></span>
+                <span style="--i:12;"></span>
+                <span style="--i:13;"></span>
+                <span style="--i:14;"></span>
+                <span style="--i:15;"></span>
+                <span style="--i:16;"></span>
+                <span style="--i:17;"></span>
+                <span style="--i:18;"></span>
+                <span style="--i:19;"></span>
+                <span style="--i:20;"></span>
+            </div>
+        </div>
+    </section>
+   
+</body>
+</html>
